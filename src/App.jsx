f94d50297c6683f2921f68e31e8f7035c5a9243c
SonCuan <sonquan2611@gmail.com>
2024-09-21 18:11:10 +0700

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,85 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import api from "./servis/axios";
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-  const [inputValueUpdate, setInputValueUpdate] = useState("");
-  const [editIndex, setEditIndex] = useState(null);
-
-  const handleAddTodo = () => {
-    if (inputValue.trim() !== "") {
-      setTodos([...todos, { text: inputValue, checked: false }]);
-      setInputValue("");
-    }
-  };
-
-  const handleUpdateTodo = (index) => {
-    if (inputValueUpdate.trim() !== "") {
-      setTodos(todos.map((todo, i) => (i === index ? inputValueUpdate : todo)));
-      setEditIndex(null);
-    }
-  };
-
-  const handleCheckboxChange = (index) => {
-    setTodos(
-      todos.map((todo, i) =>
-        i === index ? { ...todo, checked: !todo.checked } : todo
-      )
-    );
-  };
-
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    (async () => { 
+      try {
+        const res = await api.get("/products");
+        setProducts(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })()
+  }, []);
+  const handleDelete = async (id) => {
+    try {
+      if(confirm("Ban co muon xoa khong?")) {
+        await api.delete(`/products/${id}`);
+      setProducts(products.filter((item) => item.id !== id));
+      }
+    } catch (error) {
+      console.log(error);
+    } 
+  }
   return (
     <>
-      <div className="todo">
-        <input
-          type="text"
-          placeholder="Enter todo"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <button onClick={handleAddTodo}>Add</button>
-
-        <h1>Todo List</h1>
-        <ul>
-          {todos.map((todo, index) => (
-            <li key={index}>
-              {editIndex === index ? (
-                <>
-                  <input
-                    type="text"
-                    value={inputValueUpdate}
-                    onChange={(e) => setInputValueUpdate(e.target.value)}
-                  />
-                  <button onClick={() => handleUpdateTodo(index)}>Save</button>
-                  <button onClick={() => setEditIndex(null)}>Cancel</button>
-                </>
-              ) : (
-                <>
-                  <input
-                    type="checkbox"
-                    checked={todo.checked}
-                    onChange={() => handleCheckboxChange(index)}
-                  />
-                  {todo.checked ? (
-                    <del>{todo.text}</del>
-                  ) : (
-                    <p
-                      onDoubleClick={() => {
-                        setEditIndex(index);
-                        setInputValueUpdate(todo.text);
-                      }}
-                    >
-                      {todo.text}
-                    </p>
-                  )}
-                  <button
-                    onClick={() =>
-                      setTodos(todos.filter((_, i) => i !== index))
-                    }
-                  >
-                    Delete
-                  </button>
-                </>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <Routes>
-        <Route exact path="/home" element={<Home />} />
-      </Routes>
+           <form>
+  <div className="mb-3">
+    <label  className="form-label">Name</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+  </div>
+  <div className="mb-3">
+    <label className="form-label">Price</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
+  </div>
+  <div className="mb-3">
+    <label  className="form-label">Name</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+    <h1>Danh sach san pham</h1>
+    <Link to="/home/add"><button className="btn btn-success">Add</button></Link>
+    <table className="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Image</th>
+          <th>Descripton</th>
+          <th>Available</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((item) => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.price}</td>
+            <td>
+              <img
+                src={item.pictureUrl}
+                alt={item.name}
+                width={100}
+                height={100}
+              />
+            </td>
+            <td>{item.description}</td>
+            <td>{item.available ? "Co" : "Khong co"}</td>
+            <td>
+              <Link to={`home/edit/${item.id}`}><button className="btn btn-primary">Edit</button></Link>
+              <button className="btn btn-danger" onClick={() => {handleDelete(item.id)}}>Delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
     </>
   );
 }
